6e510352b7def386c9249cd051ab24a7090e973e
jackson1989-design <jackson1989@latimorelegacy.com>
2026-02-06 17:52:25 -0500

    Update ai_studio_code.js

--- ai_studio_code.js
+++ ai_studio_code.js
@@ -3,5 +3,5 @@ main: "https://jackson1989-design.github.io/latimore-life-legacy-site/",
 bannerUrl: "https://jackson1989-design.github.io/latimore-life-legacy-site/hero.jpg",
 
 // TO YOUR NEW LINK (Example):
-main: "https://latimore-legacy.netlify.app/",
+main: "https://jackson1989-design.github.io/jacksonlatimoredigitalbusinesscard/",
 bannerUrl: "./hero.jpg", // Use ./ if the image is in the same folder
