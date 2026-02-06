d8153ea0c3542e9bdadccda65c3a64da03a6d4ab
jackson1989-design <jackson1989@latimorelegacy.com>
2026-02-06 17:40:32 -0500

    Add files via upload

---
+++ ai_studio_code.js
@@ -0,0 +1,7 @@
+// CHANGE THIS:
+main: "https://jackson1989-design.github.io/latimore-life-legacy-site/",
+bannerUrl: "https://jackson1989-design.github.io/latimore-life-legacy-site/hero.jpg",
+
+// TO YOUR NEW LINK (Example):
+main: "https://latimore-legacy.netlify.app/",
+bannerUrl: "./hero.jpg", // Use ./ if the image is in the same folder
