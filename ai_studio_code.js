24dca12adc0948afe6691aa7593e7b7c6b17be5e
jackson1989-design <jackson1989@latimorelegacy.com>
2026-02-06 19:47:00 -0500

    Delete ai_studio_code.js

--- ai_studio_code.js
+++
@@ -1,7 +0,0 @@
-// CHANGE THIS:
-main: "https://jackson1989-design.github.io/latimore-life-legacy-site/",
-bannerUrl: "https://jackson1989-design.github.io/latimore-life-legacy-site/hero.jpg",
-
-// TO YOUR NEW LINK (Example):
-main: "https://jackson1989-design.github.io/jacksonlatimoredigitalbusinesscard/",
-bannerUrl: "./hero.jpg", // Use ./ if the image is in the same folder
